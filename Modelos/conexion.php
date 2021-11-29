b7679acc59f8cfba7247b34a98adbe8be13a9a7f
Jaan-8aglez <l1311ochoagjanet@gmail.com>
2021-11-28 19:46:48 -0600

    eliminacion de consulta eliminar y buscador

--- Modelos/conexion.php
+++ Modelos/conexion.php
@@ -1,4 +1,11 @@
 <?php
-	$conexion= new mysqli("localhost", "root", "", "proyecto");
-	
-?>
+	$host="localhost";
+	$bd="incendios";
+	$usuario="root";
+	$password="";
+
+	$conexion = new mysqli($host,$usuario,$password,$bd);
+	$conexion -> set_charset("utf8");
+	if($conexion -> connect_error) {
+		die("Conexion fallida");
+	}
