5fded18a5b477452a49ec4c3383019fe90055ab6
Jaan-8aglez <l1311ochoagjanet@gmail.com>
2021-11-30 21:45:16 -0600

    consultas de la tabla puesto de control

--- Vistas/estructura/menu.php
+++ Vistas/estructura/menu.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="assets/dist/img/icono-fire.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="assets/dist/img/icono-fire.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
       <span class="brand-text font-weight-light">Gestión de Incendios</span>
     </a>
 
@@ -11,7 +11,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="assets/dist/img/bombero.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Bomberos</a>
@@ -37,9 +37,9 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-home"></i>
               <p>
-                Dashboard
+                Inicio
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -239,126 +239,6 @@
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>
-                    Login & Register v1
-                    <i class="fas fa-angle-left right"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="../examples/login.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Login v1</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/register.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Register v1</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/forgot-password.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Forgot Password v1</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/recover-password.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Recover Password v1</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>
-                    Login & Register v2
-                    <i class="fas fa-angle-left right"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="../examples/login-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Login v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/register-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Register v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/forgot-password-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Forgot Password v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/recover-password-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Recover Password v2</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/lockscreen.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Lockscreen</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/legacy-user-menu.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Legacy User Menu</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/language-menu.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Language Menu</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/404.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Error 404</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/500.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Error 500</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/pace.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Pace</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../examples/blank.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Blank Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../../starter.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Starter Page</p>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
@@ -387,99 +267,17 @@
           <li class="nav-item">
             <a href="../../iframe.html" class="nav-link">
               <i class="nav-icon fas fa-ellipsis-h"></i>
-              <p>Tabbed IFrame Plugin</p>
+              <p>Diagramas</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="https://adminlte.io/docs/3.1/" class="nav-link">
               <i class="nav-icon fas fa-file"></i>
-              <p>Documentation</p>
+              <p>Documentación</p>
             </a>
           </li>
-          <li class="nav-header">MULTI LEVEL EXAMPLE</li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="fas fa-circle nav-icon"></i>
-              <p>Level 1</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-circle"></i>
-              <p>
-                Level 1
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Level 2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>
-                    Level 2
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon"></i>
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon"></i>
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon"></i>
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Level 2</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="fas fa-circle nav-icon"></i>
-              <p>Level 1</p>
-            </a>
-          </li>
-          <li class="nav-header">LABELS</li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-danger"></i>
-              <p class="text">Important</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-warning"></i>
-              <p>Warning</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
-              <p>Informational</p>
-            </a>
-          </li>
+         
+ 
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
--- Vistas/modulos/puestoControl.php
+++ Vistas/modulos/puestoControl.php
@@ -1,4 +1,10 @@
-  <!-- Content Wrapper. Contains page content -->
+<?php
+require '././Modelos/conexion.php';
+
+$resultado = $conexion->query("SELECT * FROM puestos_control")or die ($conexion->error);
+
+?>
+<!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -9,7 +15,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
               <li class="breadcrumb-item active">Puestos de Control</li>
             </ol>
           </div>
@@ -23,23 +29,76 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
+        <?php
+          if(isset($_GET['error'])){
+        ?>
+        <div class="alert alert-danger" role="alert">
+         <?php echo $_GET['error']; ?>
+         <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php  } ?>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+        <?php
+          if(isset($_GET['success'])){
+        ?>
+        <div class="alert alert-success" role="alert">
+           Se ha insertado correctamente
+         <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php  } ?>
+
+        <button type="button" class="btn btn-primary left" data-toggle="modal" data-target="#ModalInsertar">
+          <i class="fa fa-plus mr-2"></i>Agregar Puestos de Control</button>
         </div>
         <div class="card-body">
-        ¡Comienza a crear tu increíble aplicación!
-        </div>
-        <!-- /.card-body -->
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th class="text-center">Id Puesto</th>
+                    <th class="text-center" >Nombre Puesto</th>
+                    <th class="text-center" >Punto geo</th>
+                    <th class="text-center" >Frecuencia</th>
+                    <th class="text-center" >Dni guarda</th>
+                    <th class="text-center" >Acciones</th>
+                  </tr>
+                  </thead>
+                <tbody>
+                <?php
+
+                 while($fila= mysqli_fetch_array($resultado)){
+               ?>
+            <tr>
+
+             <td><?php echo $fila['id_puesto'];?></td>
+             <td><?php echo $fila['nombre_puesto'];?></td>
+             <td><?php echo $fila['punto_geo'];?></td>
+             <td><?php echo $fila['frecuencia_zona'];?></td>
+             <td><?php echo $fila['dni_guarda'];?></td>
+
+             <td><button class="btn btn-success btnEditar mr-5" 
+             data-id="<?php echo $fila['id_puesto'];?>"
+             data-nombre="<?php echo $fila['nombre_puesto'];?>"
+             data-punto_geo="<?php echo $fila['punto_geo'];?>"
+             data-frecuencia="<?php echo $fila['frecuencia_zona'];?>"
+             data-dni="<?php echo $fila['dni_guarda'];?>"
+             data-toggle="modal" data-target="#modalEditar">
+             <i class="fa fa-edit"></i></button>
+
+             <button class="btn btn-danger btnEliminar" 
+             data-id="<?php echo $fila['punto_geo'];?>"
+             data-toggle="modal" data-target="#modalEliminar">
+             <i class="fa fa-trash"></i></button></td>
+
+            </tr>
+               <?php } ?>
+                </tbody>
+                        
+                </table>
+              </div>
+              <!-- /.card-body -->
+
         <div class="card-footer">
-          Footer
+          
         </div>
         <!-- /.card-footer-->
       </div>
@@ -49,3 +108,149 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+<!-- Modal Insertar-->
+<div class="modal fade" id="ModalInsertar" tabindex="-1" aria-labelledby="ModalInsertarLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <form action="././Modelos/insertarPuestoControl.php" method="POST" enctype="multipart/form-data" > 
+      <div class="modal-header">
+        <h5 class="modal-title" id="ModalInsertarLabel">Agregar Puesto de Control</h5>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+        <div class="form-group col-6">
+          <label>Nombre Puesto:</label> 
+          <input type="text" name="nombre" placeholder="nombre" id="nombre" class="form-control" required> 
+        </div>
+        <div class="form-group col-6">
+          <label>Punto geo:</label> 
+          <input type="text" name="punto_geo" placeholder="punto geodésico" id="punto_geo" class="form-control" required> 
+        </div>
+        </div>
+        <div class="row">
+        <div class="form-group col-6">
+          <label>Frecuencia</label> 
+          <input type="text" name="frecuencia" placeholder="frecuencia zona" id="frecuencia" class="form-control" required> 
+        </div>
+        <div class="form-group col-6">
+          <label>Dni guarda</label> 
+          <input type="text" name="dni" placeholder="dni" id="dni" class="form-control" required> 
+        </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Insertar</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal Eliminar -->
+  <div class="modal fade" id="modalEliminar" tabindex="-1" aria-labelledby="modalEliminarLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document" >
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalEliminarLabel">Eliminar Puesto de Control</h5>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ¿Desea eliminar este puesto de control?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-danger eliminar" data-dismiss="modal">Eliminar</button>
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- Modal Editar -->
+  <div class="modal fade" id="modalEditar" tabindex="-1" aria-labelledby="modalEditarLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document" >
+    <div class="modal-content">
+      <form action="././Modelos/editarPuestoControl.php" method="POST" enctype="multipart/form-data" > 
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalEditarLabel">Editar Puesto de Control</h5>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <input type="hidden" id="idEdit" name="id" class="form-control" >
+        <div class="row">
+        <div class="form-group col-6">
+          <label>Nombre Puesto:</label> 
+          <input type="text" name="nombre" placeholder="nombre" id="nombreEdit" class="form-control" required> 
+        </div>
+        <div class="form-group col-6">
+          <label>Punto geo:</label> 
+          <input type="text" name="punto_geo" placeholder="punto geodésico" id="punto_geoEdit" class="form-control" required> 
+        </div>
+        </div>
+        <div class="row">
+        <div class="form-group col-6">
+          <label>Frecuencia</label> 
+          <input type="text" name="frecuencia" placeholder="frecuencia zona" id="frecuenciaEdit" class="form-control" required> 
+        </div>
+        <div class="form-group col-6">
+          <label>Dni guarda</label> 
+          <input type="text" name="dni" placeholder="DNI guarda" id="dniEdit" class="form-control" required> 
+        </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary editar">Actualizar</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- jQuery -->
+<script src="assets/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="assets/jquery-ui/jquery-ui.min.js"></script>
+<script>
+$(document).ready(function(){
+  var idEliminar= -1;
+  var idEditar= -1;
+  var fila;
+  $(".btnEliminar").click(function(){
+    idEliminar=$(this).data('id');
+    fila=$(this).parent('td').parent('tr');
+  });
+  $(".eliminar").click(function(){
+    $.ajax({
+      url: '././Modelos/eliminarPuestoControl.php',
+      method: 'POST',
+      data:{
+        id:idEliminar
+      }
+    }).done(function(res){
+      alert(res);
+      $(fila).fadeOut(1000);
+    });
+  });
+  $(".btnEditar").click(function(){
+    idEditar=$(this).data('id');
+    var nombre=$(this).data('nombre');
+    var punto_geo=$(this).data('punto_geo');
+    var frecuencia=$(this).data('frecuencia');
+    var dni=$(this).data('dni');
+    $("#idEdit").val(idEditar);
+    $("#nombreEdit").val(nombre);
+    $("#punto_geoEdit").val(punto_geo);
+    $("#frecuenciaEdit").val(frecuencia);
+    $("#dniEdit").val(dni);
+
+});
+
+});
+</script>
